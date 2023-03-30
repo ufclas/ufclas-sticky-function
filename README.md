1c0643e230ee47ae05698e64bef0a8e1a4af9dc8
suisrael <s.israel@ufl.edu>
2023-03-30 11:58:25 -0400

    Initial file upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ufclas-sticky-function
+
+Here is an example of how this could be used in combination with bootstrap styles. The relevant styles to the sticky functionality are "parent-of-sticky", "sibling-of-sticky" and "sticky":
+
+<div class="row parent-of-sticky">
+  <div class="col-sm-9 sibling-of-sticky">
+    Main page content
+  </div>
+  <div class="col-sm-3 sticky">
+    Sticky sidebar
+  <div>
+</div>
---
+++ ufclas-sticky-function.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Plugin Name: UFCLAS Sticky Function
+ * Plugin URI: https://clas.ufl.edu/
+ * Description: This plugin is for making navigation stick using sticky style.
+ * Version: 1.0
+ * Author: Fnu Ojasvi
+ * Author URI: https://commsupport.clas.ufl.edu
+ */
+
+/*==================================
+
+Set height of parent of sticky
+
+====================================*/
+
+function set_sticky_height() {
+    ?>
+    <script>
+        if (document.querySelector('.sticky')) {
+            function setStickyHeight() {
+                const parentDiv = document.querySelector('.sibling-of-sticky');
+                const parent = document.querySelector('.parent-of-sticky');
+                const parentHeight = parentDiv.getBoundingClientRect().height;
+                parent.style.height = (parentHeight) + 'px';
+            }
+            setStickyHeight();
+        }
+    </script>
+    <?php
+}
+
+add_action('wp_footer', 'set_sticky_height');
