5b4b4c7ce0b54c0b8b117f664a0bf92b2066385c
suisrael <s.israel@ufl.edu>
2023-04-24 17:00:13 -0400

    tweak js and css

--- ufclas-sticky-function.php
+++ ufclas-sticky-function.php
@@ -11,57 +11,52 @@
 
  /*==================================
 
+ Set height of parent of sticky
+
+ ====================================*/
+
+ function set_sticky_height() {
+     ?>
+     <script>
+         if (document.querySelector('.sticky')) {
+             function setStickyHeight() {
+                 const parentDiv = document.querySelector('.col-sm-9.sidebar-to-right');
+                 const parent = document.querySelector('.parent-of-sticky');
+                 const parentHeight = parentDiv.getBoundingClientRect().height;
+                 parent.style.height = (parentHeight) + 'px';
+             }
+              // Add height style only if min-width is 768px
+              if (window.innerWidth >= 768) {
+                     parent.style.height = (parentHeight) + 'px';
+                 } else {
+                     parent.style.height = 'auto';
+                 }
+             setStickyHeight();
+         }
+     </script>
+     <?php
+
+
+ }
+
+ add_action('wp_footer', 'set_sticky_height');
+
+ /*==================================
+
  Add sticky style
 
  ====================================*/
+
  function add_sticky_style() {
      ?>
      <style>
-     @media (min-width: 768px) {
-
-        .sticky {
-            position: -webkit-sticky;
-            position: sticky;
-            top: 0;
-        }
-     }
-     @media (max-width: 768px) {
-
-        .sticky {
-            display: none;
-        }
-     }
+         .sticky {
+             position: -webkit-sticky;
+             position: sticky;
+             top: 0;
+         }
      </style>
      <?php
  }
+
  add_action('wp_head', 'add_sticky_style');
-
-/*==================================
-
-Set height of parent of sticky
-
-====================================*/
-
-function set_sticky_height() {
-    ?>
-    <script>
-            if (document.querySelector('.sticky')) {
-                function setStickyHeight() {
-                    const parentDiv = document.querySelector('.col-sm-9.sidebar-to-right');
-                    const parent = document.querySelector('.parent-of-sticky');
-                    const parentHeight = parentDiv.getBoundingClientRect().height;
-                    parent.style.height = (parentHeight) + 'px';
-                }
-                 // Add height style only if min-width is 768px
-                 if (window.innerWidth >= 768) {
-                        parent.style.height = (parentHeight) + 'px';
-                    } else {
-                        parent.style.height = 'auto';
-                    }
-                setStickyHeight();
-            }
-    </script>
-    <?php
-}
-
-add_action('wp_footer', 'set_sticky_height');
