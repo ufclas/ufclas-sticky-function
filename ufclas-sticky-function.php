468020ad4a5132aaa8340d5bddc9d689677f75b7
suisrael <s.israel@ufl.edu>
2023-04-25 13:27:32 -0400

    make mobile height Auto

--- ufclas-sticky-function.php
+++ ufclas-sticky-function.php
@@ -9,7 +9,7 @@
  */
 
 
- /*==================================
+  /*==================================
 
  Set height of parent of sticky
 
@@ -20,23 +20,22 @@
      <script>
          if (document.querySelector('.sticky')) {
              function setStickyHeight() {
-                 const parentDiv = document.querySelector('.col-sm-9.sidebar-to-right');
+                 const parentDiv = document.querySelector('.sibling-of-sticky');
                  const parent = document.querySelector('.parent-of-sticky');
                  const parentHeight = parentDiv.getBoundingClientRect().height;
                  parent.style.height = (parentHeight) + 'px';
-             }
-              // Add height style only if min-width is 768px
-              if (window.innerWidth >= 768) {
+             // Add height style only if min-width is 768px
+                 if (window.innerWidth >= 768) {
                      parent.style.height = (parentHeight) + 'px';
                  } else {
                      parent.style.height = 'auto';
                  }
+               }
+
              setStickyHeight();
          }
      </script>
      <?php
-
-
  }
 
  add_action('wp_footer', 'set_sticky_height');
@@ -46,17 +45,24 @@
  Add sticky style
 
  ====================================*/
-
  function add_sticky_style() {
      ?>
      <style>
-         .sticky {
-             position: -webkit-sticky;
-             position: sticky;
-             top: 0;
-         }
+     @media (min-width: 768px) {
+
+        .sticky {
+            position: -webkit-sticky;
+            position: sticky;
+            top: 0;
+        }
+     }
+     @media (max-width: 768px) {
+
+        .sticky {
+            display: none;
+        }
+     }
      </style>
      <?php
  }
-
  add_action('wp_head', 'add_sticky_style');
